86377893c2d77df63e4f97ec7f95114a32d260e2
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-08-21 04:38:50 +0800

    feat: setup model relationships on order tracking

--- server/app/Models/Orders.php
+++ server/app/Models/Orders.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Orders extends Model
 {
@@ -26,5 +27,10 @@ class Orders extends Model
         return $this->belongsToMany(Sizes::class, 'order_quantity_size', 'order_id', 'size_id')
             ->withPivot('quantity')
             ->withTimestamps();
+    }
+
+    public function order_tracking(): HasMany
+    {
+        return $this->hasMany(OrderTracking::class, 'order_id');
     }
 }
